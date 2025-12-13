4a5425b725428084a468ae7083b716bbd7fdd54e
Florian MARIN <flomarin88@gmail.com>
2025-12-13 22:41:27 +0100

    feat(people): read an env var

--- src/main/people.ts
+++ src/main/people.ts
@@ -1,9 +1,17 @@
+import { Attio } from 'attio-js'
 import { FastAlfred } from 'fast-alfred'
 import { Variables } from '@common/variables.enum'
 
-;(() => {
+;(async () => {
     const alfredClient = new FastAlfred()
 
-    const attioApiKey: string = alfredClient.env.getEnv(Variables.API_KEY) || ''
+    const attioApiKey: string = alfredClient.env.getEnv(Variables.API_KEY, { defaultValue: '' })
     alfredClient.log('api key -> ' + attioApiKey)
+
+    const attioClient = new Attio({
+        apiKey: attioApiKey,
+    })
+
+    const people = await attioClient.objects.list()
+    console.log(people)
 })()
